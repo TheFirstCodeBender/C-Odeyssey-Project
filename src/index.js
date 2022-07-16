d93e177b6d8f54727625312f60070c6f16f2fef2
Prince Nwaonicha <lolgfman@gmail.com>
2022-07-16 18:54:10 -0500

    Added Game Timers and Game Over

--- src/index.js
+++ src/index.js
@@ -136,7 +136,38 @@ function rectangularCollision({
         rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
         )
 }
+function determineWinner(player1Health, player2Health, timerId) {
+    clearTimeout(timerId)
+    document.querySelector('#DisplayResult').style.display = 'flex'
+    if (player1Health === player2Health) {
+            document.querySelector('#DisplayResult').innerHTML = 'Tie'   
+        }
+            
+        if (player1Health > player2Health) {
+            document.querySelector('#DisplayResult').innerHTML = 'Player 1 Wins'   
+        }
+        if (player1Health < player2Health) {
+            document.querySelector('#DisplayResult').innerHTML = 'Player 2 Wins'    
+        }
+}
+let timer = 60
+let timerId
+function decreaseTimer() {
+    
+    if (timer > 0) {
+       timerId = setTimeout(decreaseTimer,1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+    if(timer === 0) {
+        
+        determineWinner(player1.health, player2.health, timerId)
+        
+    }
+    
+}
 
+decreaseTimer()
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -183,6 +214,11 @@ function animate() {
         player2.isAttacking = false
         player1.health -= 10
         document.querySelector('#Player1Health').style.width = player1.health + '%'
+    }
+
+    // end game based on health
+    if (player1.health === 0 || player2.health === 0) {
+        determineWinner(player1.health,player2.health, timerId)
     }
 }
 
